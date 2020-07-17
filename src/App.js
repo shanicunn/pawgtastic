d08ea6c69969b07b284f65959489724446296e78
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-16 23:59:32 -0400

    Created form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
-    
+    this.state = [
+      
+    ]
 
   }
   render() {
     return (
     <div className="App">
-    
+    <form>
+      <label name="name">Name:</label>
+      <input type="text" value="Pet's name"></input>
+    </form>
     </div>
   );
   }
