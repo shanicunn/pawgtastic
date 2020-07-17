1a2ebe69a4a8cff8bbf8312fdfbb71855549b77d
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 00:05:06 -0400

    Created breed field and label

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = [
+  // constructor() {
+  //   super();
+  //   this.state = [
 
-    ]
+  //   ]
 
-  }
+  // }
   render() {
     return (
     <div className="App">
     <form>
       <label name="name">Name:</label>
       <input type="text" value="Pet's name"></input>
+
       <img src="" alt="logo"></img>
       <label name="upload">Upload a photo</label>
+
+      <label name="breed">Breed:</label>
+      <input type="text" value="Pet's breed"></input>
     </form>
     </div>
   );
