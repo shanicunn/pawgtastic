7a4b0468e19c161bfcd716fffc8e43b6893632a4
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 00:10:51 -0400

    Created two colums divs and one row div

--- src/App.js
+++ src/App.js
@@ -14,17 +14,27 @@ class App extends React.Component {
     return (
     <div className="App">
     <form>
+    <div className="column1">
       <label name="name">Name:</label>
       <input type="text" value="Pet's name"></input>
       <br></br>
-      <img src="" alt="logo"></img>
-      <label name="upload">Upload a photo</label>
-      <br></br>
-      <label name="breed">Breed:</label>
+       <label name="breed">Breed:</label>
       <input type="text" value="Pet's breed"></input>
       <br></br>
-      <label name="birhday">Birthday:</label>
+
+    </div>
+      <div className="column2">
+        <img src="" alt="logo"></img>
+      <label name="upload">Upload a photo</label>
+      <br></br>
+        <label name="birhday">Birthday:</label>
       <input type="date" value="MM/DD/YY"></input>
+      </div>
+     <div className="lastRow">
+       
+     </div>
+     
+   
     </form>
     </div>
   );
