4389b9b0e05b90cb95ac2eb76293cef3cb51c203
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 00:38:34 -0400

    Added weight label

--- src/App.js
+++ src/App.js
@@ -10,31 +10,39 @@ class App extends React.Component {
   //   ]
 
   // }
+  
   render() {
+   
     return (
     <div className="App">
-    <form>
-    <div className="column1">
-      <label name="name">Name:</label>
-      <input type="text" value="Pet's name"></input>
-      <br></br>
-       <label name="breed">Breed:</label>
-      <input type="text" value="Pet's breed"></input>
-      <br></br>
-      <label name="gender">Gender:</label>
-    </div>
-      <div className="column2">
-        <img src="" alt="logo"></img>
-      <label name="upload">Upload a photo</label>
-      <br></br>
-        <label name="birhday">Birthday:</label>
-      <input type="date" value="MM/DD/YY"></input>
-      <br></br>
-      <label name="spayOrNeut">Spayed or Neutered?</label>
-      </div>
-     <div className="lastRow">
-
-     </div>
+      {/* Form */}
+      <form>
+        {/* Column 1 */}
+        <div className="column1">
+          <label name="name">Name:</label>
+          <input type="text" value="Pet's name"></input>
+          <br></br>
+          <label name="breed">Breed:</label>
+          <input type="text" value="Pet's breed"></input>
+          <br></br>
+           <label name="gender">Gender:</label>
+            
+        </div>
+        {/* Column 2 */}
+        <div className="column2">
+          <img src="" alt="logo"></img>
+          <label name="upload">Upload a photo</label>
+          <br></br>
+          <label name="birhday">Birthday:</label>
+          <input type="date" value="MM/DD/YY"></input>
+          <br></br>
+          <label name="spayOrNeut">Spayed or Neutered?</label>
+         </div>
+        {/* Last Row */}
+       <div className="lastRow">
+        <label name="weight">Weight:</label>
+        
+        </div>
      
    
     </form>
