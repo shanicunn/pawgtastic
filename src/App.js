56276ba9faf5770329c38704bf4dcf50fb32e659
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 00:06:50 -0400

    Added break tags to clean form up

--- src/App.js
+++ src/App.js
@@ -16,12 +16,13 @@ class App extends React.Component {
     <form>
       <label name="name">Name:</label>
       <input type="text" value="Pet's name"></input>
-
+      <br></br>
       <img src="" alt="logo"></img>
       <label name="upload">Upload a photo</label>
-
+      <br></br>
       <label name="breed">Breed:</label>
       <input type="text" value="Pet's breed"></input>
+      <br></br>
     </form>
     </div>
   );
